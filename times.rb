18dcd1d54400707692331fbe7dc5fb53dfeecf60
Trai Compton <tlcompton2546@outlook.com>
2017-03-01 00:38:59 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,5 +1,9 @@
-def using_times
-	7.times do
-    puts "Wingardium Leviosa"
-	end
+#USING_TIMES
+
+=begin
+	Outputs the string 'Wingardium Leviosa' 7 times by default
+=end
+
+def using_times(x = 7)
+	x.times {puts "Wingardium Leviosa"}
 end
