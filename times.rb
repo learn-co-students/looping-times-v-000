c0fd4103c16cd1332ec13da942c3fd92fb75b06d
Richard Dotson <rich@cityofla.net>
2017-10-07 05:31:55 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,14 @@
+#this method is more abstract and eloquent due to interpolation
+#def using_times
+#	levitation_charm = "Wingardium Leviosa"
+#  7.times do
+#    puts "{levitation_charm}"
+#  end
+#end
+#using_times
 def using_times
-	#your code here
-end
-
-
-
-
+  7.times do
+    puts "Wingardium Leviosa"
+  end
+  end
+using_times
