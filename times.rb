4d8e32bc41d7e84fa4711c02cb5610e93d8cb6e6
Steven Crouse <sdcrouse91@gmail.com>
2018-08-06 19:37:21 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,10 @@
 def using_times
 	#your code here
+	number = 7 
+	# Not required, but I'm writing this as a reminder that 
+	# I can send a variable to #times as long as it's an integer.
+	
+	number.times do
+	  puts "Wingardium Leviosa"
+	end
 end
-
-
-
-
